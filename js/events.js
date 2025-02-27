64ee6bac708740f545f71e343660deb4b68a0fa3
alessandrofeitoza01@gmail.com <alessandrofeitoza01@gmail.com>
2025-02-27 20:01:08 -0300

    Aula 19: Parte 03

--- js/events.js
+++ js/events.js
@@ -3,17 +3,37 @@ const SELECT_ESTADO = document.getElementById('estado');
 const SELECT_CIDADE = document.getElementById('cidade');
 
 function atualizarRegioes() {
-    let regioes = [
-        'Norte',
-        'Nordeste',
-        'Sul',
-        'Sudeste',
-        'Centro Oeste',
-    ];
+    fetch('https://servicodados.ibge.gov.br/api/v1/localidades/regioes')
+        .then(res => res.json())
+        .then(regioes => {
+            regioes = regioes.map(cada => `<option>${cada.nome}</option>`);
 
-    regioes.forEach(cadaRegiao => {
-        SELECT_REGIAO.innerHTML += `<option>${cadaRegiao}</option>`;
-    });
+            SELECT_REGIAO.innerHTML += regioes;
+        });
 }
 
-atualizarRegioes();
+function atualizarEstados() {
+    let estados = [
+        'Ceará',
+        'Maranhão',
+        'Paraiba',
+    ];
+
+    estados = estados.map(cada => `<option>${cada}</option>`);
+
+    SELECT_ESTADO.innerHTML += estados;
+}
+
+function atualizarCidades() {
+    let cidades = [
+        'Fortaleza',
+        'Paracuru',
+        'Madalena',
+    ];
+
+    cidades = cidades.map(cada => `<option>${cada}</option>`);
+
+    SELECT_CIDADE.innerHTML += cidades;
+}
+
+atualizarRegioes(); 
