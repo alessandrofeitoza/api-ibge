2ffdbf3d156350671d4b1ecff951f0ac7d68c270
alessandrofeitoza01@gmail.com <alessandrofeitoza01@gmail.com>
2025-02-27 21:35:21 -0300

    Aula 19: Parte 05

--- js/events.js
+++ js/events.js
@@ -6,40 +6,40 @@ function atualizarRegioes() {
     fetch('https://servicodados.ibge.gov.br/api/v1/localidades/regioes')
         .then(res => res.json())
         .then(regioes => {
-            regioes = regioes.map(cada => `<option>${cada.nome}</option>`);
+            regioes = regioes.map(cada => `<option value="${cada.id}">${cada.nome}</option>`);
 
             SELECT_REGIAO.innerHTML += regioes;
         });
 }
 
 function atualizarEstados() {
-    let id = 1;
+    let id = SELECT_REGIAO.value;
+
+    SELECT_ESTADO.innerHTML = '<option selected disabled> -- Selecione -- </option>';
 
     fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/regioes/${id}/estados`)
-        .then()
-        .then();
+        .then(res => res.json())
+        .then(estados => {
+            estados = estados.map(cada => `<option value="${cada.id}">${cada.nome}</option>`);
 
-    let estados = [
-        'Ceará',
-        'Maranhão',
-        'Paraiba',
-    ];
-
-    estados = estados.map(cada => `<option>${cada}</option>`);
-
-    SELECT_ESTADO.innerHTML += estados;
+            SELECT_ESTADO.innerHTML += estados;
+        });
 }
 
 function atualizarCidades() {
-    let cidades = [
-        'Fortaleza',
-        'Paracuru',
-        'Madalena',
-    ];
+    let id = SELECT_ESTADO.value;
 
-    cidades = cidades.map(cada => `<option>${cada}</option>`);
+    SELECT_CIDADE.innerHTML = '<option selected disabled> Carregando... </option>';
 
-    SELECT_CIDADE.innerHTML += cidades;
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${id}/municipios`)
+        .then(res => res.json())
+        .then(cidades => {
+            SELECT_CIDADE.innerHTML = '<option selected disabled> -- Selecione -- </option>';
+
+            cidades = cidades.map(cada => `<option>${cada.nome}</option>`);
+
+            SELECT_CIDADE.innerHTML += cidades;
+        });
 }
 
 atualizarRegioes(); 
